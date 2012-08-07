55c191a3c871a2eb3feeed2f1859468edd470365
Robert Moore <romoore@cs.rutgers.edu>
2012-08-07 15:06:44 -0400

    Updating formatting, adding new test data.

--- index.php
+++ index.php
@@ -15,31 +15,38 @@
     new Event(mktime(11,47,0,8,3,2012), "Tam finished his talk", "Big Conference Room"),
     new Event(mktime(11,31,45,8,3,2012), "Created Event Class", "Rob on his laptop"),
     new Event(mktime(11,05,37,8,3,2012), "Tam started his talk", "Big Conference Room"),
+    new Event(mktime(10,25,37,8,3,2012), "Tam is practicing his talk", "Tam's cubicle"),
+    new Event(mktime(9,42,37,8,3,2012), "Coffee was brewed", "Kitchen"),
+    new Event(mktime(8,00,00,8,3,2012), "WINLAB was unlocked", "Front door"),
   );
   
   $fakeModules = array (
-    new Module('0123242', 'Security System', True, 'Disarmed'),
-    new Module('AF23XDf', 'Presence Detector', True, '2 Users Present'),
-    new Module('532XCXA', 'Rain Detector', False, 'Last rained July 31, 2012'),
-    new Module('1949822', '142 Happy St.', True, 'Online. Last updated 30 seconds ago'),
+    new Module('0123242', 'Security System', TRUE, 'Disarmed'),
+    new Module('AF23XDf', 'Presence Detector', TRUE, '2 Users Present'),
+    new Module('532XCXA', 'Rain Detector', FALSE, 'Last rained July 31, 2012'),
+    new Module('1949822', '142 Happy St.', TRUE, 'Online. Last updated 30 seconds ago'),
   );
 
-  function printEvent($event) {
-    echo '<div class="event" title="'.$event->getExtraInfo()."\">\n";
-    echo '<span class="eventTime">'.date("H:i",$event->getTimestamp()).'</span>';
-    echo ' &mdash; '.$event->getMessage();
-    echo "</div><br />\n";
+  function printEvent(Event $event) {
+    echo '<tr title="'.$event->getExtraInfo()."\">\n";
+    echo '<td>'.date("H:i",$event->getTimestamp())."</td>\n";
+    echo '<td>'.$event->getMessage()."</td>\n";
+    echo "</tr>\n";
   }
   
-  function printModule($module) {
-    echo '<div class="module" title="'.$module->getStatus()."\">\n";
-    echo '<span class="moduleName">'.$module->getDisplayName().'</span>'."\n";
-    echo '<div id="'.$module->getId().'" class="toggle basic primary" data-enabled="ON" data-disabled="OFF" data-toggle="toggle" title="'.$module->getStatus().'">'."\n";
-    echo '<input type="checkbox" value="1" name="myCheckbox" class="checkbox" checked="checked" />'."\n";
+  function printModule(Module $module) {
+    echo "<tr>\n";
+    echo '<td>'.$module->getDisplayName()."</td>\n";
+    echo '<td>'.$module->getStatus()."</td>\n";
+    echo "<td><div class=\"module\">\n";
+    echo '<div id="'.$module->getId().'" class="toggle basic" data-enabled="ON" data-disabled="OFF" data-toggle="toggle">'."\n";
+    echo '<input type="checkbox" value="1" name="myCheckbox" class="checkbox" ';
+      if($module->isEnabled()){ echo 'checked ';}
+    echo "\" />.\n";
     echo '<label class="check" for="myCheckbox"></label>'."\n";
-    echo "</div>\n";
-    echo "</div>\n";
-    echo "<br />\n";
+    echo "</div>\n"; // Button div
+    echo "</td>\n";
+    echo "</tr>\n";
   }
 ?>
 
@@ -57,24 +64,40 @@
         <h1>Owl Platform @ <?php echo $siteName; ?></h1>
       </div>
       <div class="row">
-        <div class="span8">
+        <div class="span6">
           <h2>Recent Events</h2>
           <hr />
+          <table class="table table-striped">
+            <thead>
+              <th>When</th>
+              <th>What</th>
+            </thead>
+            <tbody>
           <?php
             foreach($fakeEvents as &$event) {
               printEvent($event);
             }
           ?>
-          
+            </tbody>
+          </table>
         </div>
-        <div class="span4">
-          <h2>Module Status</h2>          
+        <div class="span5">
+          <h2>Operating Status</h2>
           <hr />
+          <table class="table table-striped">
+            <thead>
+            <th>Name</th>
+            <th>Status</th>
+            <th>Enabled</th>
+            </thead>
+            <tbody>
           <?php
             foreach($fakeModules as &$module) {
               printModule($module);
             }
           ?>
+            </tbody>
+          </table>
         </div>
       </div>
       <hr>

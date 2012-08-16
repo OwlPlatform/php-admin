4dc762f13671b4d55c3a59f4fc98809bcf5d6b81
Robert Moore <romoore@cs.rutgers.edu>
2012-08-15 21:50:32 -0400

    Adding user/permission mockup (incomplete).

--- admin-users.php
+++ admin-users.php
@@ -32,11 +32,186 @@
         <div class="span2">
           <?php include(ABSPATH.'inc/admin-menu.php'); ?>
         </div>
-        <div class="span4 offset1">
-          <h2>Users and Permissions</h2>
-
+        <div class="span6 offset1">
+          <h2>Users</h2>
+          <table class="table table-striped">
+            <thead>
+              <th>Username</th>
+              <th>Real Name</th>
+              <th>Roles</th>
+            </thead>
+            <tbody>
+              <tr>
+                <td>jsmith81</td>
+                <td>Jane Smith</td>
+                <td>
+                  <ul>
+                    <li>Administrator</li>
+                    <li>All Notifications</li>
+                  </ul>
+                </td>
+              </tr>
+              <tr>
+                <td>bigsmithers</td>
+                <td>John Smith</td>
+                <td>
+                  <ul>
+                    <li>User</li>
+                    <li>All Notifications</li>
+                  </ul>
+                </td>
+              </tr>
+              <tr>
+                <td>jilliebean92</td>
+                <td>Jill Smith</td>
+                <td>
+                  <ul>
+                    <li>User</li>
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-      </div>
+      </div> <!-- Row -->
+      <div class="row">
+        <div class="span6 offset3">
+          <h2>Roles</h2>
+          <table class="table table-striped">
+            <thead>
+              <th>Role Name</th>
+              <th>Actions</th>
+              <th>Identifier</th>
+              <th>Attribute</th>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Administrator</td>
+                <td>
+                  <ul>
+                    <li>Read</li>
+                    <li>Create</li>
+                    <li>Update</li>
+                    <li>Expire</li>
+                    <li>Delete</li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li>[ALL]</li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li>[ALL]</li>
+                  </ul>
+                </td>
+              </tr>
+              <tr>
+                <td>All Notifications</td>
+                <td>
+                  <ul>
+                    <li>Read</li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li>[ALL]</li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li>[ALL]</li>
+                  </ul>
+                </td>
+              </tr>
+              <tr>
+                <td>User</td>
+                <td>
+                  <ul>
+                    <li>Read</li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li>$username.*</li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li>[ALL]</li>
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div> <!-- Row -->
+            <div class="row">
+        <div class="span6 offset3">
+          <h2>Permissions</h2>
+          <table class="table table-striped">
+            <thead>
+              <th>Permission Name</th>
+              <th>Actions</th>
+              <th>Identifier</th>
+              <th>Attribute</th>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Configure System</td>
+                <td>
+                  <ul>
+                    <li>Read</li>
+                    <li>Create</li>
+                    <li>Update</li>
+                    <li>Expire</li>
+                    <li>Delete</li>
+                  </ul>
+                </td>
+                <td>system.*</td>
+                <td>[ALL]</td>
+              </tr>
+              <tr>
+                <td>Update System</td>
+                <td>
+                  <ul>
+                    <li>Read</li>
+                    <li>Update</li>
+                    <li>Expire</li>
+                  </ul>
+                </td>
+                <td>system.*</td>
+                <td>[ALL]</td>
+              </tr>
+              <tr>
+                <td>Configure User</td>
+                <td>
+                  <ul>
+                    <li>Read</li>
+                    <li>Create</li>
+                    <li>Update</li>
+                    <li>Expire</li>
+                  </ul>
+                </td>
+                <td>$username.*</td>
+                <td>[ALL]</td>
+              </tr>
+              <tr>
+                <td>Monitor (Public)</td>
+                <td>
+                  <ul>
+                    <li>Read</li>
+                  </ul>
+                </td>
+                <td>[ALL]</td>
+                <td>[ALL]</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div> <!-- Row -->
+
       <hr>
 
       <footer>
